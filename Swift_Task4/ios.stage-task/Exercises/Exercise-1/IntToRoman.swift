9674e0504c11bfbfa7c0bb39d9506c5cfa066a63
Vladimir Benko <vnbenko@gmail.com>
2021-06-05 14:59:53 +0300

    feat:  implement public extension for exercise 1

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,27 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        if self <= 0 || self > 3999 {
+            return nil
+        }
+        
+        let dictionaryInt = [
+            100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M",
+            10: "X", 40: "XL", 50: "L", 90: "XC",
+            1: "I", 4: "IV", 5: "V", 9: "IX"].sorted(by: >)
+        
+        var int = self
+        var roman = ""
+        
+        for dict in dictionaryInt {
+            while int >= dict.key {
+                roman += dict.value
+                int -= dict.key
+            }
+        }
+        return roman
     }
 }
+
+
+
